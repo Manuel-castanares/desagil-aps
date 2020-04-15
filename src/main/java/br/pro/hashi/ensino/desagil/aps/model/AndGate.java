afe94071d15ca3bcef0ea46f18cea6513f7dd107
Luis Filipe Carrete <lfcsanchez@live.com>
2020-04-15 18:45:58 -0300

    Repositorio restaurado ao original

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -1,29 +1,21 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class AndGate extends Gate {
-    private final NandGate nandLeft;
-    private final NandGate nandRight;
+    private final NandGate nand;
 
     public AndGate() {
         super("AND", 2);
 
-        nandLeft = new NandGate();
-
-        nandRight = new NandGate();
-        nandRight.connect(0, nandLeft);
-        nandRight.connect(1, nandLeft);
+        nand = new NandGate();
     }
 
     @Override
     public boolean read() {
-        return nandRight.read();
+        return !(nand.read());
     }
 
     @Override
     public void connect(int inputIndex, Emitter emitter) {
-        if (inputIndex < 0 || inputIndex > 1) {
-            throw new IndexOutOfBoundsException(inputIndex);
-        }
-        nandLeft.connect(inputIndex, emitter);
+        nand.connect(inputIndex, emitter);
     }
 }
