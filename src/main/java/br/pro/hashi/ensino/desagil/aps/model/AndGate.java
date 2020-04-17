482b1a6c4d77dabdeadcbacda899c7cc3d1736d4
GustavovPierre <38561106+GustavovPierre@users.noreply.github.com>
2020-04-16 22:40:44 -0300

    Update AndGate.java
    AndGate arrumado

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -1,21 +1,30 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class AndGate extends Gate {
-    private final NandGate nand;
+    private final NandGate nand1;
+    private final NandGate nand2;
 
     public AndGate() {
         super("AND", 2);
 
-        nand = new NandGate();
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+
     }
 
     @Override
     public boolean read() {
-        return !(nand.read());
+        return nand2.read();
     }
 
     @Override
     public void connect(int inputIndex, Emitter emitter) {
+        if ((inputIndex != 0) && (inputIndex != 1)) {
+            throw new IndexOutOfBoundsException(inputIndex);
+        }
         nand.connect(inputIndex, emitter);
     }
 }
